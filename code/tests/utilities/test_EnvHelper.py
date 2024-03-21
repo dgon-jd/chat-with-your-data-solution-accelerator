0f60b0192bd065a6739ecce85e97ec678f737a85
Dmytro Honcharenko <dmytro.honcharenko@techfabric.com>
2024-03-21 16:16:21 +0200

    Renamed azure open ai base constant

--- code/tests/utilities/test_EnvHelper.py
+++ code/tests/utilities/test_EnvHelper.py
@@ -7,11 +7,11 @@ def test_openai_base_url_generates_url_based_on_resource_name_if_not_set(
 ):
     # given
     openai_resource_name = "some-openai-resource"
-    monkeypatch.delenv("OPENAI_API_BASE")
+    monkeypatch.delenv("AZURE_OPENAI_ENDPOINT")
     monkeypatch.setenv("AZURE_OPENAI_RESOURCE", openai_resource_name)
 
     # when
-    actual_open_api_base = EnvHelper().OPENAI_API_BASE
+    actual_open_api_base = EnvHelper().AZURE_OPENAI_ENDPOINT
 
     # then
     assert actual_open_api_base == f"https://{openai_resource_name}.openai.azure.com/"
@@ -19,11 +19,11 @@ def test_openai_base_url_generates_url_based_on_resource_name_if_not_set(
 
 def test_openai_base_url_uses_env_var_if_set(monkeypatch: MonkeyPatch):
     # given
-    expected_openai_api_base = "some-openai-resource-base"
-    monkeypatch.setenv("OPENAI_API_BASE", expected_openai_api_base)
+    expected_AZURE_OPENAI_ENDPOINT = "some-openai-resource-base"
+    monkeypatch.setenv("AZURE_OPENAI_ENDPOINT", expected_AZURE_OPENAI_ENDPOINT)
 
     # when
-    actual_open_api_base = EnvHelper().OPENAI_API_BASE
+    actual_open_api_base = EnvHelper().AZURE_OPENAI_ENDPOINT
 
     # then
-    assert actual_open_api_base == expected_openai_api_base
+    assert actual_open_api_base == expected_AZURE_OPENAI_ENDPOINT
